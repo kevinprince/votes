f255f06add6ff171ecd3198fb835df747892e94a
Kevin Prince <nextgengames@gmail.com>
2012-11-01 09:59:16 +0000

    adding queue for location check

---
+++ queue_tweets_geocoding.rb
@@ -0,0 +1,30 @@
+require 'bunny'
+require 'active_support/json'
+require 'mongo'
+
+j = ActiveSupport::JSON
+b = Bunny.new()
+
+db = Mongo::Connection.new("127.0.0.1", 27017).db("election")
+tweets = db.collection("tweets")
+
+b.start
+
+  q = b.queue('state_enriching')
+  
+    cursor = tweets.find()
+    
+    obj = cursor.next_document
+    
+    while obj
+      data = {
+        'id' => obj["_id"],
+        'tweet_id' => obj["id"],
+        'user_location' => obj["user"]["location"]
+      }
+      q.publish(j.encode(data))
+      obj = cursor.next_document
+    end
+
+
+b.stop
